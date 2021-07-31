74b25349d89277f60671bbbc97349ec95f865646
PR0C3SS <20180767@CE.PUCMM.EDU.DO>
2021-07-30 21:08:20 -0400

    ADD REST

---
+++ practica8-UniRest/src/main/java/Main.java
@@ -0,0 +1,99 @@
+import kong.unirest.GenericType;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int matricula;
+        String carrera;
+        String nombre;
+        int num =1;
+        do{
+            System.out.println("Menu:\n1.Agregar estudiante\n2.Buscar Estudiante\n3.Eliminar Estudiante\n4.Listar estudiantes\nSeleccione:");
+            num = sc.nextInt();
+            switch(num)
+            {
+                case 1 :
+                    System.out.println("Ingrese la matricula: ");
+                    matricula = sc.nextInt();
+                    System.out.println("Ingrese la carrera: ");
+                    carrera = sc.nextLine();
+                    System.out.println("Ingrese el nombre: ");
+                    nombre = sc.nextLine();
+                    guardarEstudiante(matricula,nombre,carrera);
+                    break; // break es opcional
+
+                case 2 :
+                    System.out.println("Ingrese la matricula: ");
+                    matricula = sc.nextInt();
+                    buscarEstudiante(matricula);
+                    break; // break es opcional
+
+                case 3 :
+                    System.out.println("Ingrese la matricula: ");
+                    matricula = sc.nextInt();
+                    eliminarEstudiante(matricula);
+                    break; // break es opcional
+
+                case 4 :
+                    buscarListaEstudiante();
+                    break; // break es opcional
+                // Podemos tener cualquier número de declaraciones de casos o case
+                // debajo se encuentra la declaración predeterminada, que se usa cuando ninguno de los casos es verdadero.
+                // No se necesita descanso en el case default
+                default :
+                    System.out.println("Opcion incorrecta");
+            }
+        }while(num != 0);
+        String cadena = sc.nextLine();
+
+
+    }
+    public static void guardarEstudiante(int mat, String nam, String car)
+    {
+
+        HttpResponse<JsonNode> estudiantePOST = Unirest.post("http://localhost:7000/api/estudiante/")
+                .header("accept", "application/json")
+                .queryString("apiKey", "123")
+                .body(new Estudiante(mat,nam,car))
+                .asJson();
+        System.out.println("El codigo de respuesta es: "+estudiantePOST.getStatus());
+        System.out.println("El mensaje es: "+estudiantePOST.getBody().toString());
+    }
+    public static void buscarEstudiante(int mat)
+    {
+        Estudiante estudianteGET = Unirest.get("http://localhost:7000/api/estudiante/"+mat)
+                .asObject(Estudiante.class)
+                .getBody();
+
+        System.out.println("matricula="+estudianteGET.getMatricula()+", Nombre="+estudianteGET.getNombre()+", carrera="+estudianteGET.getCarrera());
+    }
+    public static void eliminarEstudiante(int mat)
+    {
+        HttpResponse<Estudiante> estudianteDELETE = Unirest.delete("http://localhost:7000/api/estudiante/:"+mat)
+                .header("accept", "application/json")
+                .queryString("apiKey", "123")
+                .asObject(Estudiante.class);
+        System.out.println("El codigo de respuesta es: "+estudianteDELETE.getStatus());
+        System.out.println("El mensaje es: "+estudianteDELETE.getBody().toString());
+    }
+
+    public static void buscarListaEstudiante()
+    {
+        HttpResponse<List<Estudiante>> estudianteLISTA = Unirest.get("http://localhost:7000/api/estudiante/")
+                .header("accept", "application/json")
+                .queryString("apiKey", "123")
+                .asObject(new GenericType<List<Estudiante>>() {});
+        System.out.println("El codigo de respuesta es: "+estudianteLISTA.getStatus());
+        System.out.println("El mensaje es: "+estudianteLISTA.getBody().toString());
+    }
+
+}
